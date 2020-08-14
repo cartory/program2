6c57af985f09e509993d52c3cd6200c21a88174f
cartory <xXcartoryXx@gmail.com>
2020-08-14 19:56:37 -0400

    ejercicios cadenas&series

--- Unit1.cpp
+++ Unit1.cpp
@@ -252,6 +252,7 @@ void __fastcall TForm1::ordenarn1Click(TObject *Sender) {
 	ordenar(n);
 	ShowMessage(n);
 }
+
 // ---------------------------------------------------------------------------
 void invertir(String &x) {
 	int n = x.Length();
@@ -259,78 +260,81 @@ void invertir(String &x) {
 		if (n > 1) {
 			wchar_t a = x[1];
 			wchar_t b = x[n];
-			x.Delete(n,1);
-			x.Delete(1,1);
+			x.Delete(n, 1);
+			x.Delete(1, 1);
 			invertir(x);
 			x = String(b) + x + a;
 		}
 	}
 }
 
-void __fastcall TForm1::invertirn2Click(TObject *Sender)
-{
+void __fastcall TForm1::invertirn2Click(TObject *Sender) {
 	/*
-		""  =>      ""
-		a   =>      a
-		ab  =>      ba
-		abc =>      cba
+	 ""  =>      ""
+	 a   =>      a
+	 ab  =>      ba
+	 abc =>      cba
 
 
-		x = abcd
-		a,d
-			x = cb
-			=>  d-cb-a
-		--------
-		x = bc
-		b,c
-		=> c-""-b
-		--------
-		x = ""
-		x = "a"
-	*/
+	 x = abcd
+	 a,d
+	 x = cb
+	 =>  d-cb-a
+	 --------
+	 x = bc
+	 b,c
+	 => c-""-b
+	 --------
+	 x = ""
+	 x = "a"
+	 */
 	String x = InputBox("Invertir", "cad :", "");
 	invertir(x);
 	ShowMessage(x);
 }
-//---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
 byte contarPalabras(String x) {
 	byte c;
 	if (x == "") {
 		c = 0;
-	}else if (x.Length() == 1) {
+	}
+	else if (x.Length() == 1) {
 		c = 0;
 		if (x[1] != ' ') {
 			c++;
 		}
-	}else {
+	}
+	else {
 		c = 0;
 		if (x[1] != ' ' && x[2] == ' ') {
 			c++;
 		}
-		x.Delete(1,1);
+		x.Delete(1, 1);
 		c += contarPalabras(x);
 	}
 	return c;
 }
-void __fastcall TForm1::cantidadPalabrasx1Click(TObject *Sender)
-{
+
+void __fastcall TForm1::cantidadPalabrasx1Click(TObject *Sender) {
 	/*
-		if (x[i] != ' ' y x[i+1] == ' ')
-			c++
-		x = hola_mundo_ ?? => 2
-	*/
+	 if (x[i] != ' ' y x[i+1] == ' ')
+	 c++
+	 x = hola_mundo_ ?? => 2
+	 */
 	String x = InputBox("ContarPalabras", "cad :", "");
 	byte c = contarPalabras(x);
 	ShowMessage(c);
 }
-//---------------------------------------------------------------------------
-bool esVocal(wchar_t z){
+
+// ---------------------------------------------------------------------------
+bool esVocal(wchar_t z) {
 	/*
-		z = m   >> w.Pos(z) = 0
-		z = p   >> w.Pos(z) = 0
-		z = A   >> w.Pos(z) = 6
-		z = a   >> w.Pos(z) = 1
-	*/
+	 z = m   >> w.Pos(z) = 0
+	 z = p   >> w.Pos(z) = 0
+	 z = A   >> w.Pos(z) = 6
+	 z = a   >> w.Pos(z) = 1
+	 */
 	String w = "aeiouAEIOU";
 	return w.Pos(z) > 0;
 }
@@ -338,7 +342,7 @@ bool esVocal(wchar_t z){
 void eliminarVocales(String &x) {
 	if (x != "") {
 		wchar_t z = x[1];
-		x.Delete(1,1);
+		x.Delete(1, 1);
 		eliminarVocales(x);
 		if (!esVocal(z)) {
 			x = String(z) + x;
@@ -346,42 +350,42 @@ void eliminarVocales(String &x) {
 	}
 }
 
-void __fastcall TForm1::eliminarVocalesx1Click(TObject *Sender)
-{
+void __fastcall TForm1::eliminarVocalesx1Click(TObject *Sender) {
 	/*
-		""  ==>     ""
-		"a" ==>     ""
-		"hola " =>   "hl "
+	 ""  ==>     ""
+	 "a" ==>     ""
+	 "hola " =>   "hl "
 
-		x = ho_
-		c = h
-			c??esVocal
-			x=h_
-		----------------
-		x = o_
-		c = o
-			c??esVocal
-			=> x=_
-		----------------
-		x = _
-		c = _
-			c??esVocal
-			=> x=_
-		----------------
-		x = ""
-	*/
+	 x = ho_
+	 c = h
+	 c??esVocal
+	 x=h_
+	 ----------------
+	 x = o_
+	 c = o
+	 c??esVocal
+	 => x=_
+	 ----------------
+	 x = _
+	 c = _
+	 c??esVocal
+	 => x=_
+	 ----------------
+	 x = ""
+	 */
 	String x = InputBox("EliminarVocales", "cad :", "");
 	eliminarVocales(x);
 	ShowMessage(x);
 }
-//---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
 String palabraMasLarga(String x) {
 	int n = x.Length();
 	String palMax = "";
 	if (n > 0) {
 		int p = x.LastDelimiter(" ");
-		String pal = x.SubString(p+1, n-p);
-		x.SetLength(p-1);
+		String pal = x.SubString(p + 1, n - p);
+		x.SetLength(p - 1);
 		palMax = palabraMasLarga(x);
 		if (pal.Length() > palMax.Length()) {
 			palMax = pal;
@@ -389,91 +393,277 @@ String palabraMasLarga(String x) {
 	}
 	return palMax;
 }
-void __fastcall TForm1::palabraMasLargax1Click(TObject *Sender)
-{
-	/*
-		""      =>  ""
-		hola    =>  hola
-		pedro mundo  =>  mundo
-//
-		n = 10
-		p = 5
-		x = hola_mundo
-		p+1 = 6
-		n-p = 5
-		pal = mundo
 
-		x.setLength(4);
-			=> x = hola
-		n = 4
-		x = hola mundo
-		x.setLength(3)  =>  x = hol
-		x.setLength(2)  =>  x = ho
-		x.setLength(1)  =>  x = h
-		x.setLength(0)  =>  x =
-	*/
+void __fastcall TForm1::palabraMasLargax1Click(TObject *Sender) {
+	/*
+	 ""      =>  ""
+	 hola    =>  hola
+	 pedro mundo  =>  mundo
+	 //
+	 n = 10
+	 p = 5
+	 x = hola_mundo
+	 p+1 = 6
+	 n-p = 5
+	 pal = mundo
+
+	 x.setLength(4);
+	 => x = hola
+	 n = 4
+	 x = hola mundo
+	 x.setLength(3)  =>  x = hol
+	 x.setLength(2)  =>  x = ho
+	 x.setLength(1)  =>  x = h
+	 x.setLength(0)  =>  x =
+	 */
 	String x = InputBox("Palabra+Larga", "cad :", "");
 	String p = palabraMasLarga(x);
 	ShowMessage(p);
 }
-//---------------------------------------------------------------------------
-bool estaOrdenadoAsc(Cardinal n){
+
+// ---------------------------------------------------------------------------
+bool estaOrdenadoAsc(Cardinal n) {
 	bool h;
 	if (n < 10) {
 		h = true;
-	}else {
-		byte d = n%10;
+	}
+	else {
+		byte d = n % 10;
 		n /= 10;
-//		h = estaOrdenadoAsc(n) && !(n%10 > d);
+		// h = estaOrdenadoAsc(n) && !(n%10 > d);
 		h = estaOrdenadoAsc(n);
-		if (n%10 > d) {
+		if (n % 10 > d) {
 			h = false;
 		}
 	}
 	return h;
 }
 
-bool estaOrdenadoDesc(Cardinal n){
+bool estaOrdenadoDesc(Cardinal n) {
 	bool h;
 	if (n < 10) {
 		h = true;
-	}else {
-		byte d = n%10;
+	}
+	else {
+		byte d = n % 10;
 		n /= 10;
-//        h = estaOrdenadoAsc(n) && !(n%10 > d);
+		// h = estaOrdenadoAsc(n) && !(n%10 > d);
 		h = estaOrdenadoDesc(n);
-		if (n%10 < d) {
+		if (n % 10 < d) {
 			h = false;
 		}
 	}
 	return h;
 }
 
-void __fastcall TForm1::estaOrdenadon1Click(TObject *Sender)
-{
+void __fastcall TForm1::estaOrdenadon1Click(TObject *Sender) {
 	/*
-		1234
-		1   =>  true
-		213 =>  false
-		123 =>  true
-		12  => true
+	 1234
+	 1   =>  true
+	 213 =>  false
+	 123 =>  true
+	 12  => true
 
-		n = 123
-		---------------
-		n = 12
-		d = 3
-		h = true
-		if  d >= n %10
-			h = true
-		else
-			false
-		---------------
-		n = 1   => true
+	 n = 123
+	 ---------------
+	 n = 12
+	 d = 3
+	 h = true
+	 if  d >= n %10
+	 h = true
+	 else
+	 false
+	 ---------------
+	 n = 1   => true
 
-	*/
+	 */
 	Cardinal n = StrToInt(InputBox("EstaOrdenado", "cad :", ""));
 	bool ordenado = estaOrdenadoAsc(n) || estaOrdenadoDesc(n);
-	ShowMessage(ordenado?"true":"false");
+	ShowMessage(ordenado ? "true" : "false");
 }
-//---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
+void eliminarPrimerLetra(String &x) {
+	byte n = x.Length();
+	if (n > 0) {
+		byte p = x.LastDelimiter(" ");
+		String w = x.SubString(p + 1, n - p);
+		x.SetLength(p - 1);
+		eliminarPrimerLetra(x);
+		if (w != "") {
+			w.Delete(1, 1);
+		}
+		x = x + " " + w;
+	}
+}
+
+void __fastcall TForm1::eliminarPrimerLetrax1Click(TObject *Sender) {
+	String x = InputBox("Eliminar1erLetra", "cad :", "");
+	eliminarPrimerLetra(x);
+	ShowMessage(x);
+}
+// ---------------------------------------------------------------------------
+
+void invertirPalabras(String &x) {
+	byte n = x.Length();
+	if (n > 0) {
+		byte p = x.LastDelimiter(" ");
+		String w = x.SubString(p + 1, n - p);
+		x.SetLength(p - 1);
+		invertirPalabras(x);
+		invertir(w);
+		x = x + " " + w;
+	}
+}
+
+void __fastcall TForm1::invertirPalabras1Click(TObject *Sender) {
+	String x = InputBox("Invertir Palabras", "cad :", "");
+	invertirPalabras(x);
+	ShowMessage(x);
+}
+
+// ---------------------------------------------------------------------------
+Cardinal fibonacci(Word n) {
+	Cardinal t;
+	if (n < 2) {
+		t = n;
+	}
+	else {
+		t = fibonacci(n - 1) + fibonacci(n - 2);
+	}
+	return t;
+}
+
+void __fastcall TForm1::fibonaccit1Click(TObject *Sender) {
+	Word t = StrToInt(InputBox("Invertir Palabras", "cad :", ""));
+	Cardinal fibo = fibonacci(t);
+	ShowMessage(fibo);
+}
+// ---------------------------------------------------------------------------
+
+Cardinal serie1(Word n) {
+	Cardinal t;
+	if (n < 2) {
+		t = n;
+	}
+	else {
+		t = serie1(n - 1);
+		if (n % 2 == 0) {
+			t *= 2;
+		}
+		else {
+			t++;
+		}
+	}
+	return t;
+}
+
+void __fastcall TForm1::serie1t1Click(TObject *Sender) {
+	Word n = StrToInt(InputBox("Invertir Palabras", "cad :", ""));
+	Cardinal t = serie1(n);
+	ShowMessage(t);
+}
+
+// ---------------------------------------------------------------------------
+Cardinal sumaSerie(Word n) {
+	Cardinal s;
+	if (n < 2) {
+		s = 0;
+	}
+	else {
+		s = sumaSerie(n - 1);
+		s += (n - 1)*2;
+	}
+	return s;
+}
+
+void __fastcall TForm1::sumaSerien1Click(TObject *Sender) {
+	Word n = StrToInt(InputBox("Invertir Palabras", "cad :", ""));
+	Cardinal s = sumaSerie(n);
+	ShowMessage(s);
+}
+
+// ---------------------------------------------------------------------------
+Cardinal serie2(Word n) {
+	Cardinal t;
+	if (n < 2) {
+		t = n;
+	}
+	else {
+		t = serie2(n - 1) * 2 + 1;
+	}
+	return t;
+}
+
+void __fastcall TForm1::serie2n1Click(TObject *Sender) {
+	Word n = StrToInt(InputBox("Invertir Palabras", "cad :", ""));
+	Cardinal s = serie2(n);
+	ShowMessage(s);
+}
+
+// ---------------------------------------------------------------------------
+Cardinal sumaSerie2(Word n, Cardinal &s) {
+	Cardinal t;
+	if (n < 2) {
+		t = n;
+	}
+	else {
+		if (n % 3 == 0) {
+			t = sumaSerie2(n - 1, s) + 2;
+		}
+		else if (n % 3 == 1) {
+			t = sumaSerie2(n - 1, s) + 1;
+		}
+		else {
+			t = sumaSerie2(n - 1, s) * 2;
+		}
+		if (t % 2 == 0) {
+			s += t;
+		}
+	}
+	return t;
+}
+
+void __fastcall TForm1::sumaSerie2n1Click(TObject *Sender) {
+	Word n = StrToInt(InputBox("Suma Serie2", "cad :", ""));
+	Cardinal s = 0;
+	Cardinal t = sumaSerie2(n, s);
+	ShowMessage(s);
+}
+
+// ---------------------------------------------------------------------------
+byte cantidadPares(Cardinal n) {
+	byte c;
+	if (n < 10) {
+		c = (n % 2 == 0) ? 1 : 0;
+	}
+	else {
+		c = cantidadPares(n / 10) + cantidadPares(n % 10);
+	}
+	return c;
+}
+
+void __fastcall TForm1::DigitosParesn1Click(TObject *Sender) {
+	Cardinal n = StrToInt(InputBox("Cantidad Digitos Pares", "cad :", ""));
+	byte c = cantidadPares(n);
+	ShowMessage(c);
+}
+
+// ---------------------------------------------------------------------------
+Word sumaPares(Cardinal n) {
+	Word s;
+	if (n < 10) {
+		s = (n % 2 == 0) ? n : 0;
+	}
+	else {
+		s = sumaPares(n / 10) + sumaPares(n % 10);
+	}
+	return s;
+}
+
+void __fastcall TForm1::SumaDigitosParesn1Click(TObject *Sender) {
+	Cardinal n = StrToInt(InputBox("Cantidad Digitos Pares", "cad :", ""));
+	Word s = sumaPares(n);
+	ShowMessage(s);
+}
+// ---------------------------------------------------------------------------
